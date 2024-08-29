48429ad3adcb4f310ca7afd8944935aaa74759c3
Otávio Luque <37673813+mecoxeco@users.noreply.github.com>
2024-08-29 02:53:09 -0300

    .

--- src/deck/dto/create-deck.dto.ts
+++
@@ -1,7 +0,0 @@
-import { ApiProperty } from '@nestjs/swagger';
-
-export class CreateDeckDto {
-    @ApiProperty({ example: 'Nicol Bolas', description: 'The name of the commander' })
-    readonly commanderName: string;
-
-}
